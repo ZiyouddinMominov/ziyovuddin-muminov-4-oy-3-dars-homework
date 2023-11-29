61120f509429f869726fbe1010446e7de6d6d0e9
ZiyouddinMominov <muminovz2009@gmail.com>
2023-11-29 21:41:29 +0500

    ifga oid masalalar

---
+++ js/app.js
@@ -0,0 +1,184 @@
+// 1 - TAYYOR
+// let a = 73;
+// let b = 45;
+// let res;
+// if (a > b) {
+//   res = a / 10 - (a % 10) / 10;
+// } else {
+//   res = b / 10 - (b % 10) / 10;
+// }
+// console.log(res);
+
+// 2 - TAYYOR
+// let a = +prompt("3-xonali son kiriting:");
+// let b = parseInt(a);
+
+// let c = b % 10;
+// let d = Math.floor((b % 100) / 10);
+// let e = Math.floor(b / 100);
+
+// let f = Math.min(e, d, c);
+// console.log(`Eng kichik raqam: ${f}`);
+
+//  3 - TAYYOR
+// let a = +prompt("Son kiriting");
+// let b = (a % 100) / 10 - (a % 10) / 10;
+// let max;
+
+// if (b % 3 == 0) {
+//   max = "rost";
+// } else {
+//   max = "yolg'on";
+// }
+
+// console.log(max);
+
+// 4 - TAYYOR
+// let a = +prompt("Son kiriting");
+// let b = +prompt("Son kiriting");
+// let c = +prompt("Son kiriting");
+// let res;
+// if (a < b) {
+//   res = a ** 3;
+// } else {
+//   res = b ** 3;
+// }
+// if (c < res) {
+//   res = c ** 3;
+// }
+// console.log(res);
+
+// 5 - TAYYOR
+// let son1 = +prompt("Son kiriting");
+// let son2 = +prompt("Son kiriting");
+// let son3 = +prompt("Son kiriting");
+// let res;
+
+// if (son1 > son2) {
+//   res = son1;
+// } else {
+//   res = son2;
+// }
+// if (son3 > res) {
+//   res = son3;
+// }
+// console.log(res);
+
+// 6 - TAYYOR
+// let son1 = +prompt("Son kiriting");
+// let son2 = +prompt("Son kiriting");
+// let son3 = +prompt("Son kiriting");
+// let res;
+// if (son1 > son2) {
+//   res = son2;
+// } else {
+//   res = son1;
+// }
+// if (son3 > res) {
+//   res = son3;
+// }
+// console.log(res);
+
+// 7 - TAYYOR
+// let son1 = +prompt("Birinchi sonni kiriting:");
+// let son2 = +prompt("Ikkinchi sonni kiriting:");
+
+// if (son1 > son2) {
+//   console.log("Eng katta son: " + son1);
+// } else if (son2 > son1) {
+//   console.log("Eng katta son: " + son2);
+// } else {
+//   console.log("Sonlar teng, ularning yig'indisi ham " + (son1 + son2));
+// }
+
+// 8 - TAYYOR
+// let son1 = 27;
+// let son2 = 21;
+// let son3 = 19;
+// let son4 = 18;
+// let son5 = 90;
+// let res;
+// // ONE IF START
+// if (son1 % 3 == 0) {
+//   console.log(`3ga karrali ${son1}`);
+// } else {
+//   console.log(`3ga karrali emas ${son1}`);
+// }
+// // TWO IF START
+// if (son2 % 3 == 0) {
+//   console.log(`3ga karrali ${son2}`);
+// } else {
+//   console.log(`3ga karrali emas ${son2}`);
+// }
+// // THREE IF START
+// if (son3 % 3 == 0) {
+//   console.log(`3ga karrali ${son3}`);
+// } else {
+//   console.log(`3ga karrali emas ${son3}`);
+// }
+// // FOUR IF START
+// if (son4 % 3 == 0) {
+//   console.log(`3ga karrali ${son4}`);
+// } else {
+//   console.log(`3ga karrali emas ${son4}`);
+// }
+// // FIVE IF START
+// if (son5 % 3 == 0) {
+//   console.log(`3ga karrali ${son5}`);
+// } else {
+//   console.log(`3ga karrali emas ${son5}`);
+// }
+
+// 9 - TAYYOR
+// let son1 = 27;
+// let son2 = 21;
+// let son3 = 19;
+// let son4 = 18;
+// let res;
+// // ONE IF START
+// if (son1 % 3 == 0) {
+//   console.log(`3ga karrali ${son1}`);
+// } else {
+//   console.log(`3ga karrali emas ${son1}`);
+// }
+// if (son1 % 5 == 0) {
+//   console.log(`5ga karrali ${son1}`);
+// } else {
+//   console.log(`5ga karrali emas ${son1}`);
+// }
+// // TWO IF START
+// if (son2 % 3 == 0) {
+//   console.log(`3ga karrali ${son2}`);
+// } else {
+//   console.log(`3ga karrali emas ${son2}`);
+// }
+// if (son2 % 5 == 0) {
+//   console.log(`5ga karrali ${son2}`);
+// } else {
+//   console.log(`5ga karrali emas ${son2}`);
+// }
+// // THREE IF START
+// if (son3 % 3 == 0) {
+//   console.log(`3ga karrali ${son3}`);
+// } else {
+//   console.log(`3ga karrali emas ${son3}`);
+// }
+// if (son3 % 5 == 0) {
+//   console.log(`5ga karrali ${son3}`);
+// } else {
+//   console.log(`5ga karrali emas ${son3}`);
+// }
+// // FOUR IF START
+// if (son4 % 3 == 0) {
+//   console.log(`3ga karrali ${son4}`);
+// } else {
+//   console.log(`3ga karrali emas ${son4}`);
+// }
+// if (son4 % 5 == 0) {
+//   console.log(`5ga karrali ${son4}`);
+// } else {
+//   console.log(`5ga karrali emas ${son4}`);
+// }
+
+// 10, 11, 12 - TUSHUNMADIM USTOZ AGAR BILSEZ https://t.me/zmominov TUSHUNTIRIB BERA OLASIZMI ?
+// ILTIMOS TUSHUNTIRIB BERIN USTOZ https://t.me/zmominov2
